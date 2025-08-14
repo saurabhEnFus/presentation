5ded88b53df39b4f0757e80e276878dbc0d0f0ea
saurabhEnFus <saurabh.mane@enfuse-solutions.com>
2025-08-14 17:37:10 +0530

    logic changed

--- blocks/presentation/presentation.js
+++ blocks/presentation/presentation.js
@@ -2,44 +2,84 @@ import '../../scripts/reveal.js/dist/reveal.js';
 import RevealHighlight from '../../scripts/reveal.js/plugin/highlight/highlight.esm.js'
 
 export default function decorate(block) {
+
+
+  function welcomeSlide(row) {
+    const [classNameEl, titileEl, descFirstEl, descSecondEl] = row.children
+    return `
+    <section class="">
+      ${titileEl?.innerHTML}
+      ${descFirstEl?.innerHTML}
+      ${descSecondEl?.innerHTML}
+    </section>
+    `
+  }
+
+  function bannerSlide(row) {
+    const [classNameEl, titileEl, descFirstEl, descSecondEl] = row.children
+    const imgSrc = titileEl?.querySelector('img').src
+    return `
+    <section data-background-image=${imgSrc}>
+    </section>
+    `
+  }
+
+  function listViewSlide(row) {
+    const [classNameEl, titileEl, descFirstEl, descSecondEl] = row.children
+    const listItems = descFirstEl?.querySelectorAll('ul li');
+
+    const ulElement = document.createElement('ul');
   
+    listItems?.forEach((item) => {
+      const li = item;
+      li.classList.add('fragment')
+      ulElement.append(li)
+    });
 
-const [...slides] = block.children;
+    return `
+    <section class="">
+      ${titileEl.innerHTML}
+      ${ulElement.innerHTML}
+    </section>
+    `
+  }
 
-const mainDiv = document.createElement('div');
-[...block.children].forEach((row) => {
-    const section = document.createElement('section');
-    row.className = row.lastElementChild.textContent
-    row.removeChild(row.lastElementChild);
+  function thankyouSlide(row) {
+    const [classNameEl, titileEl, descFirstEl, descSecondEl] = row.children
+    return `
+    <section class="">
+      ${titileEl?.innerHTML}
+      ${descFirstEl?.innerHTML}
+      ${descSecondEl?.innerHTML}
+    </section>
+    `
+  }
 
-    //banner slide
-    const picture = row.querySelector('.sl-banner picture');
+  const mainDiv = document.createElement('div');
+  [...block.children].forEach((row) => {
+    let className = row.firstElementChild.textContent
+    let section;
 
-    if (picture) {
-      const imgSrc = picture.querySelector('img').src;
-      section.setAttribute('data-background-image', imgSrc);
-      row.removeChild(row.firstElementChild)
+    switch (className) {
+      case "slide-welcome":
+        section = welcomeSlide(row);
+        mainDiv.insertAdjacentHTML('beforeend', section);
+        break;
+      case "slide-banner":
+        section = bannerSlide(row);
+        mainDiv.insertAdjacentHTML('beforeend', section);
+        break;
+      case "slide-listview":
+        section = listViewSlide(row);
+        mainDiv.insertAdjacentHTML('beforeend', section);
+        break;
+      case "slide-thankyou":
+        section = thankyouSlide(row);
+        mainDiv.insertAdjacentHTML('beforeend', section);
+        break;
+      default:
+        console.log("Unknown row...");
     }
-
-    // list slide
-    const listItems = row.querySelectorAll('.sl-list ul li');
-    console.log(listItems);
-    
-
-    if (listItems) {
-      const ulElement = document.createElement('ul');
-  
-      listItems.forEach((item) => {
-         const li = item;
-         li.classList.add('fragment')
-         ulElement.append(li)
-      });
-      row.append(ulElement) 
-    }
-    
-
-    section.append(row);
-    mainDiv.append(section);
   });
 
   let slide = `
